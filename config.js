2a2d7fda1996d66e0115e70193822efefbe28387
Stackbit Code Editor <support@stackbit.com>
2021-02-08 15:12:34 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-fractal/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Anubhav Srivastava',
-  subHeading: 'Full Stack Web Developer & ',
+  subHeading: 'Full Stack Web Developer & Mentor ',
   // social
   socialLinks: [
     {
