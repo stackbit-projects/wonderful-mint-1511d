221aff2fafd84bd21bb51805b11dd737903876e1
UmerSK <umer.kdev@gmail.com>
2021-02-08 15:03:45 +0000

    Footer.js: updated Paragraph

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ export default function Footer() {
         })}
       </ul>
       <p className="copyright">
-        &copy; Umer Khan <a href="http://html5up.net">HTML5 UP</a>
+        &copy;  Umer Khan <a href="http://html5up.net">HTML5 UP</a>
       </p>
     </footer>
   );
